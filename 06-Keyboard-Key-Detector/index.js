55c59646b64545e40411f9c478e8c80760fdf246
anasrazaofficial <anasbukhari4114@gmail.com>
2023-09-30 08:58:22 -0700

    Compile project-06 (Keyboard-Key-Detector)

---
+++ 06-Keyboard-Key-Detector/index.js
@@ -0,0 +1,15 @@
+document.querySelector('body').addEventListener('keyup', (e) => {
+  console.log(e);
+  document.querySelector('div').innerHTML = `<table>
+  <tr>
+    <th>Key</th>
+    <th>Key Code</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>`
+})
