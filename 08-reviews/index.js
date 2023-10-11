a4e67b47812e8a9b2aa367927a089f34fe37b7a2
anasrazaofficial <anasbukhari4114@gmail.com>
2023-10-11 16:46:48 +0500

    Change reviews images path

--- 08-reviews/index.js
+++ 08-reviews/index.js
@@ -3,25 +3,25 @@ let users = [
     name: 'Elizabeth',
     job: 'Architecture',
     review: 'I recently installed the Smart Home Thermostat in my house, and it has been a game-changer. The ease of controlling the temperature from my phone is fantastic. The energy-saving features have noticeably reduced my monthly bills. The only reason I am not giving it five stars is because the initial setup was a bit tricky.',
-    imgSrc: 'http://127.0.0.1:5500/08-reviews/assets/user-1.jpg'
+    imgSrc: 'file:///D:/My-Repos/js-projects/08-reviews/assets/user-1.jpg'
   },
   {
     name: 'Tony',
     job: 'Software Engineer',
     review: 'I had an incredible dining experience at The Gourmet Delight. The ambiance was charming, and the staff was attentive. The food was a culinary masterpiece; each dish was a burst of flavor. I highly recommend the chef is tasting menu if you want to indulge in a gastronomic journey.',
-    imgSrc: 'http://127.0.0.1:5500/08-reviews/assets/user-2.jpg'
+    imgSrc: 'file:///D:/My-Repos/js-projects/08-reviews/assets/user-2.jpg'
   },
   {
     name: 'Chris',
     job: 'Doctor',
     review: '"Infinite Adventure" had an interesting concept, but it fell short in execution. The visuals were stunning, and the action scenes were thrilling, but the plot lacked depth and left me with unanswered questions. It is an entertaining watch if you are into sci-fi, but do not expect a masterpiece.',
-    imgSrc: 'http://127.0.0.1:5500/08-reviews/assets/user-3.jpg'
+    imgSrc: 'file:///D:/My-Repos/js-projects/08-reviews/assets/user-3.jpg'
   },
   {
     name: 'Mike',
     job: 'Manager',
     review: '"The Enigmatic Echo" is a literary gem. The author is writing style is captivating, and the characters are so well-developed that I felt a deep connection with them. The plot had unexpected twists, keeping me hooked from start to finish. I could not put it down and highly recommend it to fellow bookworms.',
-    imgSrc: 'http://127.0.0.1:5500/08-reviews/assets/user-4.jpg'
+    imgSrc: 'file:///D:/My-Repos/js-projects/08-reviews/assets/user-4.jpg'
   },
   {
     name: 'Steve',
@@ -36,7 +36,7 @@ let i = 0
 let name = document.querySelector('h4')
 let job = document.querySelector('#job')
 let review = document.querySelector('#review')
-let imgSrc = document.querySelector('img') 
+let imgSrc = document.querySelector('img')
 
 assignValues()
 
